ab120ec3cb24396e872c200a37d168a8e09ab6da
Albert <r.albert.huang@gmail.com>
2021-07-11 14:36:39 +0800

    format

--- test.ts
+++ test.ts
@@ -4,39 +4,39 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 async function test() {
-    const sdk = await makeSdk([
-        {
-            name: "UserService",
-            handler: {
-                graphql: {
-                    endpoint: `${process.env.USER_SERVICE_URL}/graphql`,
-                },
-            },
+  const sdk = await makeSdk([
+    {
+      name: "UserService",
+      handler: {
+        graphql: {
+          endpoint: `${process.env.USER_SERVICE_URL}/graphql`,
         },
-        {
-            name: "CourseService",
-            handler: {
-                graphql: {
-                    endpoint: `${process.env.COURSE_SERVICE_URL}/graphql`
-                }
-            }
-        }
-    ]);
-    const { createUser: user } = await sdk.createUserMutation({
-        createUserInput: {
-            schoolId: 1,
-            username: `Test${Math.floor(Math.random() * 1000)}`,
-            usertype: 4,
-            nickname: "Test",
-            realname: "Test",
-            email: "",
-            mobile: "",
-            avatar: "",
-            password: "Test",
+      },
+    },
+    {
+      name: "CourseService",
+      handler: {
+        graphql: {
+          endpoint: `${process.env.COURSE_SERVICE_URL}/graphql`,
         },
-    });
+      },
+    },
+  ]);
+  const { createUser: user } = await sdk.createUserMutation({
+    createUserInput: {
+      schoolId: 1,
+      username: `Test${Math.floor(Math.random() * 1000)}`,
+      usertype: 4,
+      nickname: "Test",
+      realname: "Test",
+      email: "",
+      mobile: "",
+      avatar: "",
+      password: "Test",
+    },
+  });
 
-    console.log(user.username);
+  console.log(user.username);
 }
 
 test();
